031966eb08b899e1e18919ce93553b13698f59e4
CHRIS <CHRIS@192.168.1.10>
2016-04-08 17:47:42 +0200

    Tests

---
+++ test/controller/ControlPairsTest.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author CHRIS
+ */
+public class ControlPairsTest {
+    
+    public ControlPairsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class ControlPairs.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        String question = "";
+        String answer = "";
+        ControlPairs instance = new ControlPairs();
+        instance.add(question, answer);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of size method, of class ControlPairs.
+     */
+    @Test
+    public void testSize() {
+        System.out.println("size");
+        ControlPairs instance = new ControlPairs();
+        int expResult = 0;
+        int result = instance.size();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getRandomQuestion method, of class ControlPairs.
+     */
+    @Test
+    public void testGetRandomQuestion() {
+        System.out.println("getRandomQuestion");
+        ControlPairs instance = new ControlPairs();
+        String expResult = "";
+        String result = instance.getRandomQuestion();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDifficultyRating method, of class ControlPairs.
+     */
+    @Test
+    public void testGetDifficultyRating() {
+        System.out.println("getDifficultyRating");
+        ControlPairs instance = new ControlPairs();
+        double expResult = 0.0;
+        double result = instance.getDifficultyRating();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of checkGuess method, of class ControlPairs.
+     */
+    @Test
+    public void testCheckGuess() {
+        System.out.println("checkGuess");
+        String question = "";
+        String guess = "";
+        ControlPairs instance = new ControlPairs();
+        boolean expResult = false;
+        boolean result = instance.checkGuess(question, guess);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of lookup method, of class ControlPairs.
+     */
+    @Test
+    public void testLookup() {
+        System.out.println("lookup");
+        String question = "";
+        ControlPairs instance = new ControlPairs();
+        String expResult = "";
+        String result = instance.lookup(question);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of load method, of class ControlPairs.
+     */
+    @Test
+    public void testLoad() {
+        System.out.println("load");
+        String filename = "";
+        ControlPairs instance = new ControlPairs();
+        boolean expResult = false;
+        boolean result = instance.load(filename);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of save method, of class ControlPairs.
+     */
+    @Test
+    public void testSave() {
+        System.out.println("save");
+        String filename = "";
+        ControlPairs instance = new ControlPairs();
+        boolean expResult = false;
+        boolean result = instance.save(filename);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of clear method, of class ControlPairs.
+     */
+    @Test
+    public void testClear() {
+        System.out.println("clear");
+        ControlPairs instance = new ControlPairs();
+        instance.clear();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
